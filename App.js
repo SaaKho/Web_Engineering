2fd0ba294ef42a4f9047fde1b081a14c35160242
Malik Saadan Khokhar <109228868+SaaKho@users.noreply.github.com>
2023-03-28 03:14:13 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Carousel from './Component/Carousel';
+import Booking from './Component/Booking';
+import Question2 from './Component/Question2';
+
+// const name = "Saadan";
+// const age = 23;
+
+function App() {
+  return (
+    <>
+      {/* <Carousel name={name} age={age}/> */}
+      <Carousel />
+      <Question2 />
+    </>
+
+  );
+}
+
+export default App;
