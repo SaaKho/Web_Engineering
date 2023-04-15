7ebfc73f158736b7efcafaa9319856460b0c4b84
Malik Saadan Khokhar <109228868+SaaKho@users.noreply.github.com>
2023-04-15 16:19:13 +0500

    Add files via upload
    Signed-off-by: Malik Saadan Khokhar  <109228868+SaaKho@users.noreply.github.com>

--- AdminDashboard.js
+++ AdminDashboard.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import './AdminDashboard.css'
+import React from 'react';
+import './AdminDashboard.css';
 
 export default function AdminDashboard() {
     return (
         <div>
-            <header>
-                Hotel Management System - Admin Dashboard
-            </header>
-            <div class="container">
+            <header>Hotel Management System - Admin Dashboard</header>
+            <div class="content">
                 <div class="sidebar">
                     <div class="profile">
-                        <img src="6.jpg" alt="Admin profile picture"/>
-                            <div class="profile-info">
-                                <p><strong>Username:</strong> Saadan Khokhar</p>
-                            </div>
+                        <img src="6.jpg" alt="Admin profile picture" />
+                        <div class="profile-info">
+                            <p>
+                                <strong>Username:</strong> Saadan Khokhar
+                            </p>
+                        </div>
                     </div>
                     <a href="#">Add Room</a>
                     <a href="#">Update Room</a>
                     <a href="#">Delete Room</a>
                     <a href="#">Manage Feedback</a>
                     <a href="#">Manage Reservations</a>
-                    <a href="#" id="logout">Update Profile</a>
-                    <a href="#" id="logout">Logout</a>
+                    <a href="#" id="logout">
+                        Update Profile
+                    </a>
+                    <a href="#" id="logout">
+                        Logout
+                    </a>
                 </div>
-                <main>
-                    <h1>Welcome, Admin!</h1>
-                </main>
             </div>
         </div>
-
     );
 }
