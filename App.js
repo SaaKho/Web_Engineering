810764198ebb00f41c17f651d1da69cad4443a8f
Malik Saadan Khokhar <109228868+SaaKho@users.noreply.github.com>
2023-04-17 16:52:13 +0500

    Add files via upload
    Signed-off-by: Malik Saadan Khokhar  <109228868+SaaKho@users.noreply.github.com>

--- App.js
+++ App.js
@@ -2,7 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Carousel from './Component/Carousel';
 import Booking from './Component/Booking';
-import Question2 from './Component/Question2';
+// import Sum from './Component/Sum';
+import AboutUs from './Component/AboutUs';
+import GuestReviews from './Component/GuestReviews';
+import Footer from './Component/Footer';
+import ContactUs from './Component/ContactUs';
+import FeedbackForm from './Component/FeedbackForm';
+import Testimonial from './Component/Testimonial';
+import AdminDashboard from './Component/AdminDashboard';
+
+
 
 // const name = "Saadan";
 // const age = 23;
@@ -11,8 +20,17 @@ function App() {
   return (
     <>
       {/* <Carousel name={name} age={age}/> */}
-      <Carousel />
-      <Question2 />
+      {/* <Carousel /> */}
+      {/* <Sum /> */}
+      {/* < AboutUs /> */}
+      {/* < GuestReviews /> */}
+      {/* < ContactUs /> */}
+      {/* < FeedbackForm /> */}
+      {/* <Testimonial/> */}
+      {/* < Footer />  */}
+      <AdminDashboard/>
+
+
     </>
 
   );
