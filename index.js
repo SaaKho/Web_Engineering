3c4620e2fb29e1e23a8cebf1d872803848bee474
Malik Saadan Khokhar <109228868+SaaKho@users.noreply.github.com>
2023-05-16 23:18:56 +0500

    Add files via upload
    Signed-off-by: Malik Saadan Khokhar  <109228868+SaaKho@users.noreply.github.com>

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 const dbName = 'Hotel';
 const connection_string = process.env.CONNECTION_STRING;
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5500;
 
 mongoose.connect(connection_string, {
     useNewUrlParser: true,
@@ -26,14 +26,41 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-const User = mongoose.model('User', userSchema, 'User');
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my Hotel Management System');
+const blogSchema = new mongoose.Schema({
+    blogName: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    blogContent: {
+        type: String,
+        required: true,
+    },
+    comments: [
+        {
+            type: String,
+        },
+    ],
 });
 
+
+const Blog = mongoose.model('Blog', blogSchema, 'Blog');
+
+
+const User = mongoose.model('User', userSchema, 'User');
+
+/*Tested and Verifiied*/
+app.get('/', (req, res) => {
+    res.send('Welcome to my Blog Maintaining Application');
+});
+
+/*Tested and Verifiied*/
 app.get('/getUsers', (req, res) => {
-    //User.find is used to get all the users from the database
+    //User.find is used to get all the blogs from the database
     User.find({})
         .then((users) => {
             console.log(users);
@@ -45,6 +72,7 @@ app.get('/getUsers', (req, res) => {
         });
 });
 
+/*Tested and Verifiied*/
 app.post('/signUp', (req, res) => {
     const { name, email, password } = req.body;
     const newUser = new User({ name: name, email: email, password: password });
@@ -59,6 +87,7 @@ app.post('/signUp', (req, res) => {
         });
 });
 
+/*Tested and Verifiied*/
 app.post('/login', (req, res) => {
     const { email, password } = req.body;
 
@@ -86,6 +115,141 @@ app.post('/login', (req, res) => {
         });
 });
 
-app.listen(5000, () => {
-    console.log('Listening on port 5000');
+
+/*Tested and Verifiied*/
+app.put('/updateUser/:id', async (req, res) => {
+    try {
+        const userId = '6457dc6d346f4a60fe69d3fe';
+        const { name, email } = req.body;
+
+        // Perform the update operation
+        const updatedUser = await User.findByIdAndUpdate(userId, { name, email }, { new: true });
+
+        if (!updatedUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update user' });
+    }
+});
+
+/*Tested and Verifiied*/
+app.delete('/deleteUser/:id', async (req, res) => {
+    try {
+        const userId = '6460fabb17c7714c10034230';
+
+        // Perform the delete operation
+        const deletedUser = await User.findByIdAndDelete(userId);
+
+        if (!deletedUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.json(deletedUser);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete user' });
+    }
+});
+
+/*-----------------------------CRUD FOR BLOGS---------------------------------------*/
+
+/*Tested and Verified*/
+app.get('/getBlogs', (req, res) => {
+    Blog.find({})
+        .then((blogs) => {
+            res.json(blogs);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Error retrieving blogs from database');
+        });
+});
+
+// Create new blog
+/*Tested and Verified */
+app.post('/createBlogs', (req, res) => {
+    const { blogName, author, blogContent } = req.body;
+    const newBlog = new Blog({ blogName, author, blogContent });
+    newBlog.save()
+        .then(() => {
+            res.status(201).send('Blog created successfully');
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Error creating blog');
+        });
+});
+
+// Read a blog
+/*Tested and Verified */
+app.get('/readBlogs', (req, res) => {
+    Blog.find()
+        .then((blogs) => {
+            res.json(blogs);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Error retrieving blogs from the database');
+        });
+});
+
+
+// Update a blog
+/*Tested and Verified */
+app.put('/updateBlogs/:id', async (req, res) => {
+    const id = '';
+    const { blogName, author, blogContent } = req.body;
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(id, { blogName, author, blogContent }, { new: true });
+        if (!updatedBlog) {
+            return res.status(404).json({ error: 'Blog not found' });
+        }
+        res.json(updatedBlog);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update blog' });
+    }
+});
+
+// Delete a blog
+/*Tested and Verified */
+app.delete('/deleteBlogs/:id', async (req, res) => {
+    const id = '';
+    try {
+        const deletedBlog = await Blog.findByIdAndDelete(id);
+        if (!deletedBlog) {
+            return res.status(404).json({ error: 'Blog not found' });
+        }
+        res.json(deletedBlog);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete blog' });
+    }
+});
+
+
+app.post('/CommentonBlog/:id', async (req, res) => {
+    const postId = req.params.id; // Access the post ID from the URL parameter
+    const comment = req.body.comment; // Assuming the comment is sent in the request body
+
+    try {
+        // Find the blog post by ID and update the comments array
+        const blogPost = await Blog.findById(postId);
+        if (!blogPost) {
+            return res.status(404).json({ error: 'Blog post not found' });
+        }
+
+        blogPost.comments.push(comment); // Add the comment to the comments array
+        await blogPost.save(); // Save the updated blog post
+
+        res.status(200).json({ message: 'Comment added successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+app.listen(5500, () => {
+    console.log('Listening on port 5500');
 });
